ccabb49be0173ca600e5e585088cfe2568d4708e
mohsina rahman <mohsinarahman@mohsinas-imac>
2018-04-27 21:51:09 -0700

    Adding view controllers for entering location and link

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController,MKMapViewDelegate
+{
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        let regionRadius: CLLocationDistance = 1000
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+        // Create an annotation
+        var annotation = MKPointAnnotation()
+
+        let lat = CLLocationDegrees(21.283)
+        let long = CLLocationDegrees(-157.823)
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        annotation.coordinate = coordinate
+        annotation.title = "Mohsina Rahman"
+        annotation.subtitle = "www.google.com"
+        
+        mapView.addAnnotation(annotation)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        let reuseId = "Pin1"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil
+        {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView?.pinTintColor = .green
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    
     }
-
-
+   
 }
 
